4fd5abc6bd60ddf61baf47e54f73cc4a93245b4a
Eleazar Pereira <Eleazar.pereira.p@gmail.com>
2024-09-21 23:53:57 -0300

    ingreso de models cliente, producto, categoria, venta y detalle de venta

--- Gen2/urls.py
+++ Gen2/urls.py
@@ -17,8 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from app_gen.views import miProimeraVista
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('cliente/', include('cliente.urls')),
+    path('cliente/', miProimeraVista),
 
 ]
--- app_gen/models.py
+++ app_gen/models.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
-class Customer(models.Model):
+class Client(models.Model):
     names         = models.CharField(max_length=150, verbose_name='Nombres')
     dni           = models.CharField(max_length=150, unique=True, verbose_name='Dni')
     date_creation = models.DateTimeField(auto_now=True)
@@ -18,27 +18,57 @@ class Customer(models.Model):
         return self.names
     
     class Meta:
-        verbose_name        = 'Empresa'
-        verbose_name_plural = 'Empresas'
-        db_table            = 'empresa'
+        verbose_name        = 'Cliente'
+        verbose_name_plural = 'Clientes'
+        db_table            = 'cliente'
         ordering            = ['dni']
-
-
-class Categoria(models.Model):
-    pass
+class Category(models.Model):
+    name         = models.CharField(max_length=150, verbose_name='Nombre')
 
     def __str__(self):
-        return self.names
+        return self.name
     
     class Meta:
         verbose_name        = 'Categoria'
         verbose_name_plural = 'Categorias'
         db_table            = 'categoria'
-        ordering            = ['dni']
-
-class Producto(models.Model):
+        # ordering            = ['dni']
+class Product(models.Model):
     code          = models.CharField(max_length=150, unique=True, verbose_name='Codigo')
     description   = models.CharField(max_length=150, verbose_name='Descripcion')
-    names         = models.CharField(max_length=150, verbose_name='Nombres')
-    amount        = models.IntegerField(max_length=20 , verbose_name='Cantidad')
-    price         = models.IntegerField(max_length=15 , verbose_name='Precio')
+    name         = models.CharField(max_length=150, verbose_name='Nombres')
+    amount        = models.IntegerField(verbose_name='Cantidad')
+    price         = models.IntegerField(verbose_name='Precio')
+
+    def __str__(self):
+        return self.name 
+    class Meta:
+        verbose_name        = 'Producto'
+        verbose_name_plural = 'Productos'
+        db_table            = 'producto'
+class Sale(models.Model):
+    cli         = models.ForeignKey(Client, on_delete=models.CASCADE)
+    date_joined = models.DateField(default=datetime.now)
+    subtotal    = models.DecimalField(default=0.00, max_digits=9 , decimal_places=2)
+    iva         = models.DecimalField(default=0.00, max_digits=9 , decimal_places=2)
+    total       = models.DecimalField(default=0.00, max_digits=9 , decimal_places=2)
+
+    def __str__(self):
+        return self.cli.names 
+    class Meta:
+        verbose_name        = 'Venta'
+        verbose_name_plural = 'Ventas'
+        db_table            = 'venta'
+class DetSale(models.Model):
+    sale           = models.ForeignKey(Sale, on_delete=models.CASCADE)
+    prod           = models.ForeignKey(Product, on_delete=models.CASCADE)
+    price          = models.DecimalField(default=0.00, max_digits=9 , decimal_places=2)
+    cant           = models.IntegerField(default=0)
+    subtotal       = models.DecimalField(default=0.00, max_digits=9 , decimal_places=2)
+
+    def __str__(self):
+        return self.prod.name
+    class Meta:
+        verbose_name        = 'Detalle de Venta'
+        verbose_name_plural = 'Detalle de Ventas'
+        db_table            = 'detventa'
